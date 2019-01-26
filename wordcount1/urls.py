6b9e897021e68d430b89f28a0f083f69887e045b
Shreyash J Sartandel <sartandelshreyash2@gmail.com>
2019-01-26 20:42:55 +0530

    changed homepage to home

--- wordcount1/urls.py
+++ wordcount1/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.urls import path
 from . import views
 urlpatterns = [
-     path('',views.homepage,name='home'),
+     path('',views.home,name='home'),
      path('count/',views.count,name='count'),
      path('about/',views.about,name='about')
 ]
